d72a9a5963a3265a13c83af7bb7e40b625ae11df
Jamily <jamily751@hotmail.com>
2024-02-06 10:53:46 -0300

    adicinando condicionais de viagem

--- listas.js
+++ listas.js
@@ -1,17 +1,17 @@
-console.log(Trabalhando com listas);
-const salvador = Salvador;
-const saoPaulo = São Paulo;
-const rioDeJaneiro = Rio de Janeiro;
+console.log('Trabalhando com listas');
+const salvador = 'Salvador';
+const saoPaulo = 'São Paulo';
+const rioDeJaneiro = 'Rio de Janeiro';
 
 const listaDeDestinos = new Array (
-    Salvador,
-    São Paulo,
-    Rio de Janeiro
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro'
 );
 
-listaDeDestinos.push(Curitiba);
+listaDeDestinos.push('Curitiba');
 
-listaDeDestinos.push(João Pessoa);
+listaDeDestinos.push('João Pessoa');
 
 listaDeDestinos.splice(1,2);
 console.log(listaDeDestinos[0], listaDeDestinos[2]);
